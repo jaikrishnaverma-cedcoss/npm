639eb47cf02009d9ec2bc902ca314823aae60b5d
jaikrishnaverma <jaikrishnaverma>
2022-08-31 12:32:09 +0530

    live suggestion update

--- mark01/A-React Work/01_firstReactApp/reactSearchSuggestions/suggestion/src/component/Searching.jsx
+++ mark01/A-React Work/01_firstReactApp/reactSearchSuggestions/suggestion/src/component/Searching.jsx
@@ -4,36 +4,37 @@ class Searching extends Component {
     constructor(props) {
         super(props);
 
-    this.state = {data:["jai","aman","anshika","rita pal","sudha","vaishali"],value:"",search:[]}
+        this.state = { data: ["jai", "aman", "anshika", "rita pal", "sudha", "vaishali"], value: "", search: [] }
     }
-    changeVal=(event)=>
-    {
-        this.setState({value:event.target.value},()=>{
-            
-            this.state.search=this.state.data.filter((item) => {
-            return (
-              item.toLowerCase().includes(this.state.value)
-            );
-          })
-        this.setState({search:this.state.search})
+    changeVal = (event) => {
+        this.setState({ value: event.target.value }, () => {
+
+            this.state.search = this.state.data.filter((item) => {
+                return (
+                    item.toLowerCase().includes(this.state.value)
+                );
+            })
+            this.setState({ search: this.state.search })
         })
     }
-    selectedval=(event)=>
-    {
-        this.setState({value:event.target.innerText});
+    selectedval = (event) => {
+        this.setState({ value: event.target.innerText });
     }
-    
-    render() { 
-        return ( 
+    clearer=(event)=>
+    {
+        this.setState({ value: "" });
+    }
+    render() {
+        return (
             <>
-          
-            <div style={{width:"30%"}}>
-            <div id="inpu"><input type="text" onChange={this.changeVal} value={this.state.value}/> <button>X</button></div>
-            <Suggestion arr={this.state.search} selectedval={this.selectedval} searchTerm={this.state.value}/>
-               </div>
+
+                <div style={{ width: "30%" }}>
+                    <div id="inpu"><input type="text" onChange={this.changeVal} value={this.state.value} /> <button onClick={this.clearer}>X</button></div>
+                    <Suggestion arr={this.state.search} selectedval={this.selectedval} searchTerm={this.state.value} />
+                </div>
             </>
-         );
+        );
     }
 }
- 
+
 export default Searching;
